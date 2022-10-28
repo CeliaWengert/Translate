fb074c0c38ac64ff944f6ef324dfd3d4f1240760
CeliaWengert <48057624+CeliaWengert@users.noreply.github.com>
2022-10-28 22:31:33 +0200

    Update translate_streamlit.py

--- translate_streamlit.py
+++ translate_streamlit.py
@@ -22,8 +22,7 @@ st.markdown(
     footer {visibility: hidden;}
     </style>""", unsafe_allow_html=True)
     
-st.title('Excel file translator')
-
+st.markdown(f'<p style="color:#83C9FF;font-size:100px;">{"Excel file translator"}</p>', unsafe_allow_html=True)
 
 languages = {'af': 'afrikaans','sq': 'albanian', 
 'am': 'amharic', 'ar': 'arabic', 
@@ -79,90 +78,120 @@ def range_char(start, stop):
         return list(range(start, stop+1))
     else : 
         return [chr(n) for n in range(ord(start), ord(stop)+1)]
+        
+def dataframe_viz(df):
+    df_viz=df
+    cols_viz=[]
+    df_viz.loc[-1] = df_viz.columns  # adding a row
+    df_viz.index = df_viz.index + 1  # shifting index
+    df_viz.sort_index(inplace=True) 
+    
+    for i in range(df_viz.shape[1]):
+        cols_viz.append(chr(i+65))
+    df_viz=df_viz.set_axis(cols_viz, axis=1)
+    return df_viz.head(5).fillna('')
+    
 
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+uploaded_files = st.file_uploader("Choose an XLSX or CSV file", accept_multiple_files=True)
 
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
+    
+    st.write("Preview:")
+    st.dataframe(dataframe_viz(pd.read_excel(uploaded_file)))
+    
     df = pd.read_excel(uploaded_file)
-    st.dataframe(df.head(3))
 
     inputlang = st.selectbox('Source language',languages.values(),index=20,key = "1_1")
     outputlang = st.selectbox('Output language',languages.values(),index=26,key = "1_2")
-    inputcolumn = st.text_input('List of columns to translate A-E/A,E or 1-5/1,5', 'A',key = "1_3")
-    inputcolumn=str(inputcolumn)
-    st.write(inputcolumn)
+    inputcolumn = st.text_input('List of columns to translate   (for example : 1,5/A,E or A-E/1-5 for a range of columns)', 'A',key = "1_3")
+    replacecolumn = st.selectbox('Writting option',['Overwrite','Append'],index=1,key = "1_4")
 
- 
-    if (inputcolumn.count(',')>0) and (str(inputcolumn).count('-')==0):
-        columnslist = inputcolumn.split(',')
-    elif (inputcolumn.count('-')==1) and (inputcolumn.count(',')==0):
-        start=inputcolumn.split('-')[0]
-        stop=inputcolumn.split('-')[1]
-        columnslist = range_char(start,stop)
-    elif (str(inputcolumn).count('-')==0) and (str(inputcolumn).count(',')==0):
-        columnslist =inputcolumn
-    else :
-        st.write('Error')
+    if st.button('Translate !'):
     
-    columnlist_temp=[]
-    for i in columnslist:
-        if i.isnumeric():
-            columnlist_temp.append(int(i)-1)
-        else : 
-            columnlist_temp.append(ord(i)-65)    
-    
-    try :
-        
-        columnslist=columnlist_temp
-    
-        timestr = time.strftime("%Y.%m.%d-%H.%M.%S")
-        flag = 1
-        
-        t1=time.perf_counter()
+        col1,col2=st.columns([0.1,2])
 
-        for lang_code,lang in languages.items():
-            if outputlang in lang:
-                flag=1
-                translator = Translator()
-                columns_=df.iloc[:,columnslist]
-                
-                df=df.fillna(0)
-
-                for i,c in enumerate(columns_):
-                    df["Translated "+c+" to "+lang]=df[c].map(lambda x: translator.translate(x, src=inputlang, dest=lang_code).text)
-                    
-                st.dataframe(df.head(3))
+        with col1:
+            gif_runner = st.image('fox.gif')
             
-                t2=time.perf_counter()
+        with col2:
+            st.markdown('')
+            placeholder = st.empty()
+            placeholder.markdown('In progress...', unsafe_allow_html=True)
+ 
+        if (inputcolumn.count(',')>0) and (str(inputcolumn).count('-')==0):
+            columnslist = inputcolumn.split(',')
+        elif (inputcolumn.count('-')==1) and (inputcolumn.count(',')==0):
+            start=inputcolumn.split('-')[0]
+            stop=inputcolumn.split('-')[1]
+            columnslist = range_char(start,stop)
+        elif (str(inputcolumn).count('-')==0) and (str(inputcolumn).count(',')==0):
+            columnslist =inputcolumn
+        else :
+            st.write('Error')
+        
+        columnlist_temp=[]
+        for i in columnslist:
+            if i.isnumeric():
+                columnlist_temp.append(int(i)-1)
+            else : 
+                columnlist_temp.append(ord(i)-65)    
+        
+        try :
+            
+            columnslist=columnlist_temp
+        
+            timestr = time.strftime("%Y.%m.%d-%H.%M.%S")
+            flag = 1
+            
+            t1=time.perf_counter()
 
-                st.write('Translation complete!')
-                st.write('Execution time:'+str(round(t2-t1,1))+'s.')
-                
-                def to_excel(df):
-                    output = BytesIO()
-                    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-                    df.to_excel(writer, index=False, sheet_name='Translated')
-                    workbook = writer.book
-                    worksheet = writer.sheets['Translated']
+            for lang_code,lang in languages.items():
+                if outputlang in lang:
+                    flag=1
+                    translator = Translator()
+                    columns_=df.iloc[:,columnslist]
                     
-                    writer.save()
-                    processed_data = output.getvalue()
-                    return processed_data
+                    if replacecolumn=='Overwrite':
+                        for i,c in enumerate(columns_):
+                            df[c]=df[c].map(lambda x: translator.translate(x, src=inputlang, dest=lang_code).text)
+                    else :
+                        for i,c in enumerate(columns_):
+                            df["Translated "+c+" to "+lang]=df[c].map(lambda x: translator.translate(x, src=inputlang, dest=lang_code).text)
+                        
+                    st.write(df.head(5), use_container_width=True)
                     
-                df_xlsx = to_excel(df)
+                    placeholder.empty()
+                    gif_runner.empty()
                 
-                from datetime import datetime
-                now=datetime.now()
+                    t2=time.perf_counter()
 
-                st.download_button(label='📥 Download Current Data',
-                                            data=df_xlsx ,
-                                            file_name= 'Translated_'+str(uploaded_file.name)[:-5]+'_'+str(now.year)+'-'+str(now.month).zfill(2)+'-'+str(now.day).zfill(2)+'.xlsx')
+                    st.write('Translation complete!')
+                    st.write('Execution time:'+str(round(t2-t1,1))+'s.')
+                    
+                    def to_excel(df):
+                        output = BytesIO()
+                        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+                        df.to_excel(writer, index=False, sheet_name='Translated')
+                        workbook = writer.book
+                        worksheet = writer.sheets['Translated']
+                        
+                        writer.save()
+                        processed_data = output.getvalue()
+                        return processed_data
+                        
+                    df_xlsx = to_excel(df)
+                    
+                    from datetime import datetime
+                    now=datetime.now()
 
-            else:
-                flag=0
-                continue
-    except Exception as e :
-        st.write('FATAL ERROR : ',e)
+                    st.download_button(label='📥 Download Current Data',
+                                                data=df_xlsx ,
+                                                file_name= 'Translated_'+str(uploaded_file.name)[:-5]+'_'+str(now.year)+'-'+str(now.month).zfill(2)+'-'+str(now.day).zfill(2)+'.xlsx')
+
+                else:
+                    flag=0
+                    continue
+        except Exception as e :
+            st.write('FATAL ERROR : ',e)
 
