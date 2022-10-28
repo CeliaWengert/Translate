5ba350644b109c3cf8e98069bde619587b80e9d4
CeliaWengert <48057624+CeliaWengert@users.noreply.github.com>
2022-10-28 18:05:49 +0200

    Update translate_streamlit.py

--- translate_streamlit.py
+++ translate_streamlit.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_excel(r'./NLDone.xlsx')
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 
-st.dataframe(df)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
+
+    df = pd.read_excel(uploaded_file)
+
+    st.dataframe(df)
